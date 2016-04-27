f3e36ffe73f0ec72f9590c376569fe2e4b7abb75
Daniel Harabor <dharabor@gmail.com>
2016-04-27 11:25:20 +1000

    added jps_sssp

--- src/warthog.cpp
+++ src/warthog.cpp
@@ -48,7 +48,7 @@ help()
 {
 	std::cerr << "valid parameters:\n"
     << "--format [ grid | wgm ]; followed by\n"
-	<< "\t--alg [astar | jps | jps2 | jps+ | jps2+ | jps | sssp ]\n"
+	<< "\t--alg [astar | jps | jps2 | jps+ | jps2+ | jps | jps2_sssp | sssp ]\n"
 	<< "\t--map [map filename] (only with dimacs format)\n"
 	<< "\t--scen [scenario filename]\n"
 	<< "\t--gen [map filename] (only with grid format)\n"
@@ -257,6 +257,48 @@ run_jps(warthog::scenario_manager& scenmgr)
 		}
 
 		std::cout << i<<"\t" << "jps" << "\t" 
+		<< astar.get_nodes_expanded() << "\t" 
+		<< astar.get_nodes_generated() << "\t"
+		<< astar.get_nodes_touched() << "\t"
+		<< astar.get_search_time()  << "\t"
+		<< len << "\t" 
+		<< scenmgr.last_file_loaded() << std::endl;
+
+		check_optimality(len, exp);
+	}
+	std::cerr << "done. total memory: "<< astar.mem() + scenmgr.mem() << "\n";
+}
+
+void
+run_jps2_sssp(warthog::scenario_manager& scenmgr)
+{
+    warthog::gridmap map(scenmgr.get_experiment(0)->map().c_str());
+	warthog::jps2_expansion_policy expander(&map);
+	//warthog::octile_heuristic heuristic(map.width(), map.height());
+	warthog::zero_heuristic heuristic;
+
+	warthog::flexible_astar<
+		//warthog::octile_heuristic,
+        warthog::zero_heuristic,
+	   	warthog::jps2_expansion_policy> astar(&heuristic, &expander);
+	astar.set_verbose(verbose);
+
+	std::cout << "id\talg\texpd\tgend\ttouched\ttime\tcost\tsfile\n";
+	for(unsigned int i=0; i < scenmgr.num_experiments(); i++)
+	{
+		warthog::experiment* exp = scenmgr.get_experiment(i);
+
+		int startid = exp->starty() * exp->mapwidth() + exp->startx();
+		int goalid = exp->mapwidth() * exp->mapheight();
+		double len = astar.get_length(
+				map.to_padded_id(startid),
+			   	map.to_padded_id(goalid));
+		if(len == warthog::INF)
+		{
+			len = 0;
+		}
+
+		std::cout << i<<"\t" << "jps2_sssp" << "\t" 
 		<< astar.get_nodes_expanded() << "\t" 
 		<< astar.get_nodes_generated() << "\t"
 		<< astar.get_nodes_touched() << "\t"
@@ -630,6 +672,11 @@ run_grid(warthog::util::cfg& cfg)
         { 
             //run_astar(scenmgr); 
         }
+    }
+   
+    if(alg == "jps2_sssp")
+    {
+        run_jps2_sssp(scenmgr);
     }
 }
 
