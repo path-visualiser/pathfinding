ae21fdf32d2673574a4886c73ddfbd57a4bd61fe
Daniel Harabor <dharabor@gmail.com>
2017-02-02 10:50:27 +1100

    added a forgotten header added gitignore file

---
+++ src/util/binary.h
@@ -0,0 +1,24 @@
+#ifndef WARTHOG_BINARY_H
+#define WARTHOG_BINARY_H
+
+// binary.h
+//
+// Binary search stuff
+//
+// @author: dharabor
+// @created: 2017-01-10
+//
+
+namespace warthog
+{
+
+namespace util
+{
+
+bool binary_search(uint32_t target, 
+bin
+
+}
+
+}
+
