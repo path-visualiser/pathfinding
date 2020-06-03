a93a00a97889b60ac6218b91f69d41ff56e66210
Daniel Harabor <dharabor>
2020-06-03 16:19:35 +1000

    patched with updates from ch-branch commit 38d6a04

--- anyangle/polyanya/helpers/cfg.cpp
+++ anyangle/polyanya/helpers/cfg.cpp
@@ -8,35 +8,65 @@ warthog::util::cfg::~cfg()
 {
 }
 
-void
+void 
 warthog::util::cfg::parse_args(int argc, char** argv, warthog::util::param params[])
 {
-    int current_opt;
-    for(    int c = getopt_long(argc, argv, "", params, &current_opt) ;
-            c != -1 ;
-            c = getopt_long(argc, argv, "", params, &current_opt) )
-    {
+    parse_args(argc, argv, "", params);
+}
+
+void 
+warthog::util::cfg::parse_args(int argc, char** argv, const char* options,
+        warthog::util::param params[])
+{
+	int current_opt;
+	for(    int c = getopt_long(argc, argv, options, params, &current_opt) ;
+            c != -1 ; 
+            c = getopt_long(argc, argv, options, params, &current_opt) )
+	{
         if(c == '?') break;
         if(params[current_opt].has_arg == 0)
         {
-            params_[params[current_opt].name] = "1";
+            params_[params[current_opt].name].push_back("1");
         }
-        else if(!(optarg == 0))
+        if(!(optarg == 0))
         {
-                params_[params[current_opt].name] = optarg;
+            params_[params[current_opt].name].push_back(optarg);
+            for(int index = optind; 
+                    index < argc && *argv[index] != '-'; 
+                    index++)
+            {
+                params_[params[current_opt].name].push_back(argv[index]);
+            }
         }
-    }
+	}
 }
 
 std::string
 warthog::util::cfg::get_param_value(std::string param_name)
 {
-    std::string ret("");
-    std::map<std::string, std::string>::iterator it =
-        params_.find(param_name);
-    if(it != params_.end())
-    {
-        ret =  (*it).second;
+	std::string ret("");
+	std::map<std::string, std::vector<std::string>>::iterator it = 
+		params_.find(param_name);
+	if(it != params_.end())
+	{
+        if((*it).second.size() > 0)
+        {
+            ret = (*it).second.front();
+            (*it).second.erase((*it).second.begin());
+        }
+	}
+	return ret;
+}
+
+uint32_t
+warthog::util::cfg::get_num_values(std::string param_name)
+{
+    uint32_t ret = 0;
+	std::map<std::string, std::vector<std::string>>::iterator it = 
+		params_.find(param_name);
+	if(it != params_.end())
+	{
+        ret = (uint32_t)(*it).second.size();
     }
     return ret;
 }
@@ -44,10 +74,28 @@ warthog::util::cfg::get_param_value(std::string param_name)
 void
 warthog::util::cfg::print(std::ostream& out)
 {
-    out << "cfg\n";
-    for(std::map<std::string, std::string>::iterator it = params_.begin();
-            it != params_.end(); it++)
+	out << "cfg\n";
+	for(std::map<std::string, std::vector<std::string>>::iterator it = params_.begin();
+			it != params_.end(); it++)
+	{
+		out << (*it).first <<"=";
+        for(uint32_t i = 0; i < (*it).second.size(); i++)
+        {
+             out << (*it).second.at(i);
+        }
+	}
+}
+
+void
+warthog::util::cfg::print_values(const std::string& option, std::ostream& out)
+{
+    std::map<std::string, std::vector<std::string>>::iterator it 
+        = params_.find(option);
+    if(it != params_.end())
     {
-        out << (*it).first <<"="<<(*it).second<<std::endl;
+        for(uint32_t i = 0; i < (*it).second.size(); i++)
+        {
+            out << " " << (*it).second.at(i);
+        }
     }
 }
--- anyangle/polyanya/helpers/cfg.h
+++ anyangle/polyanya/helpers/cfg.h
@@ -1,9 +1,11 @@
-#pragma once
+#ifndef WARTHOG_UTIL_CFG_H
+#define WARTHOG_UTIL_CFG_H
 
 #include "getopt.h"
 
 #include <iostream>
 #include <map>
+#include <vector>
 
 // cfg.h
 //
@@ -23,27 +25,39 @@ typedef struct option param;
 
 class cfg
 {
-    public:
-        cfg();
-        ~cfg();
+	public:
+		cfg();
+		~cfg();
 
-        void 
-        parse_args(int argc, char** argv, warthog::util::param params[]);
-
-        std::string
-        get_param_value(std::string);
-
+		void 
+		parse_args(int argc, char** argv, warthog::util::param params[]);
 
         void
-        print(std::ostream& out);
+		parse_args(int argc, char** argv, const char* short_opts, 
+                warthog::util::param params[]);
 
-    private:
-        // no copy
-        cfg(const cfg& other) {}
-        cfg& operator=(const cfg& other) { return *this; }
+		std::string
+		get_param_value(std::string);
 
-        std::map<std::string, std::string> params_;
+        uint32_t
+        get_num_values(std::string);
+
+		void
+		print(std::ostream& out);
+
+        void
+        print_values(const std::string&, std::ostream& out);
+
+	private:
+		// no copy
+		cfg(const cfg& other) {}
+		cfg& operator=(const cfg& other) { return *this; }
+
+		std::map<std::string, std::vector<std::string>> params_;
 };
 
 }
 }
+
+#endif
+
