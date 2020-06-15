e35f67f5af58f1057712f18dbcf781645baa399d
Arthur Mahéo <arthur.maheo@monash.edu>
2020-06-16 09:54:11 +1000

    Add sub-optimal options for CPD search to 'roadhog'

--- src/programs/roadhog.cpp
+++ src/programs/roadhog.cpp
@@ -918,6 +918,35 @@ run_cpd_search(warthog::util::cfg& cfg,
         warthog::pqueue_min>
             alg(&h, &expander, &open);
 
+    // Set options for CPD searh
+    std::stringstream ss;
+    std::string scale = cfg.get_param_value("fscale");
+    std::string tlim = cfg.get_param_value("uslim");
+    double f_scale;
+    uint32_t us_lim;
+
+    if (scale != "")
+    {
+        ss << scale;
+        ss >> f_scale;
+
+        if (f_scale > 0.0)
+        {
+            alg.set_quality_cutoff(f_scale);
+        }
+    }
+
+    if (tlim != "")
+    {
+        ss << tlim;
+        ss >> us_lim;
+
+        if (us_lim > 0)
+        {
+            alg.set_max_us_cutoff(us_lim);
+        }
+    }
+
     run_experiments(&alg, alg_name, parser, std::cout);
 }
 
@@ -1193,6 +1222,8 @@ main(int argc, char** argv)
 		{"noheader",  no_argument, &suppress_header, 1},
 		{"input",  required_argument, 0, 1},
 		{"problem",  required_argument, 0, 1},
+        {"fscale", required_argument, 0, 1},
+        {"uslim", required_argument, 0, 1},
 		{0,  0, 0, 0}
 	};
 
